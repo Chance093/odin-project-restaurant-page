859cea2d9bbc28dcc77a2ad9867b1e85584069aa
Chance093 <chance.dev093@gmail.com>
2022-09-13 16:59:50 -0700

    Add generateContactPage() function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import generateHomePage from './home';
 import generateMenuPage from './menu';
+import generateContactPage from './contact';
 
 // generateHomePage();
 // generateMenuPage();
+generateContactPage();
